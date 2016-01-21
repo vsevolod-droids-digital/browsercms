7ec8b990353f119802867e668e601a0ee8ce1d39
Chad <v@droids.digital>
2016-01-21 18:45:35 +0300

    Initial self branch

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "3.5.8"
+  VERSION = "0.0.1"
 
   # Return the current version of the CMS.
   def self.version
