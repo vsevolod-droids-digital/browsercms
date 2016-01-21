f91cea1453ed3411c026a94296069c2f945aaf28
Chad <v@droids.digital>
2016-01-21 18:36:07 +0300

    Test updating from git and bundle

--- lib/cms/version.rb
+++ lib/cms/version.rb
@@ -2,7 +2,7 @@
 # Allows the precise version of BrowserCMS to be determined programatically.
 #
 module Cms
-  VERSION = "3.5.7"
+  VERSION = "3.5.8"
 
   # Return the current version of the CMS.
   def self.version
